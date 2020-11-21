24d0812f497d94c4aee4bf07f3f98a544fb4d99e
mauroolsen <mauro123olsen@gmail.com>
2020-11-21 20:11:25 -0300

    Discount list script correction

--- Views/Discount/list.php
+++ Views/Discount/list.php
@@ -64,7 +64,7 @@
 
 <script>
 $(document).ready(function() {
-    $('#theaters-list').DataTable({
+    $('#discount-list').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
         }
